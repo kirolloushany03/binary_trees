b238d80e6f0c7d77770e2485852f320e2722b0f6
ali-hy <ali.haitham.ymy@gmail.com>
2024-03-29 01:05:16 +0200

    refactor 102

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -40,6 +40,8 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (!tree)
+	{
 		return (0);
+	}
 	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -5,39 +5,22 @@ int binary_tree_is_complete_rec(const binary_tree_t *tree,
 								size_t index, size_t size);
 
 /**
- * binary_tree_is_complete - Checks if a binary tree is complete
- * @tree: Pointer to the root node of the tree
- *
- * This function checks whether the binary tree rooted at the specified node
- * is a complete binary tree. A complete binary tree is a binary tree in which
- * every level, except possibly the last, is completely filled, and all nodes
- * are as far left as possible.
- *
- * @tree: Pointer to the root node of the tree
- *
- * Return: 1 if the binary tree is complete, 0 otherwise or if the tree is NULL
+ * binary_tree_is_complete - check if the binary tree is complete
+ * @tree: root of the binary tree
+ * Return: if the binary tree is complete, 1, else 0
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
 	return (tree && binary_tree_is_complete_rec(tree, 0, binary_tree_size(tree)));
 }
+
 /**
- * binary_tree_is_complete_rec - Helper function to check
- * if a binary tree is complete
- * @tree: Pointer to the root node of the tree
- * @index: Current index of the node being checked
- * @size: Total number of nodes in the tree
- *
- * This recursive function checks if the binary tree rooted
- * at the specified node is a complete binary tree by traversing
- * the tree in a level-order manner and checking
- * if all nodes are present and in the correct order.
- *
- * @tree: Pointer to the root node of the tree
- * @index: Current index of the node being checked
- * @size: Total number of nodes in the tree
- *
- * Return: 1 if the binary tree is complete, 0 otherwise
+ * binary_tree_is_complete_rec - helper function to help with checking tree is
+ * complete if a binary tree is complete
+ * @tree: root of the tree
+ * @index: idnex of node to check
+ * @size: size of the tree
+ * Return: 1 if the binary tree is complete, else 0
  */
 int binary_tree_is_complete_rec(const binary_tree_t *tree,
 								size_t index, size_t size)
@@ -50,17 +33,8 @@ int binary_tree_is_complete_rec(const binary_tree_t *tree,
 			binary_tree_is_complete_rec(tree->right, index * 2 + 2, size));
 }
 /**
- * binary_tree_size - Measures the size of a binary tree
- * @tree: Pointer to the root node of the tree
- *
- * This function measures the size of the binary tree
- * rooted at the specified node.
- * The size of a binary tree is defined as
- * the total number of nodes in the tree.
- * The size of an empty tree is 0.
- *
- * @tree: Pointer to the root node of the tree
- *
+ * binary_tree_size - get the size of a binary tree
+ * @tree: root of the tree
  * Return: Size of the binary tree, or 0 if the tree is NULL
  */
 size_t binary_tree_size(const binary_tree_t *tree)
