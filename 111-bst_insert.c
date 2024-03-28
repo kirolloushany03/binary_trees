fb5be6a796aa09fb1aeaf1b30b044138d41ce2bc
ali-hy <ali.haitham.ymy@gmail.com>
2024-03-29 01:16:04 +0200

    111

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -5,7 +5,7 @@
  * @tree: A double pointer to the root node of the BST to insert the value.
  * @value: The value to store in the node to be inserted.
  *
- * Return: A pointer to the created node, or NULL on failure.
+ * Return: the new node or null
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
