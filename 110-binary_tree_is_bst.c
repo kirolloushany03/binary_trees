580d5e2bb77bac2173293420e86787d8a734883c
ali-hy <ali.haitham.ymy@gmail.com>
2024-03-29 01:14:15 +0200

    110

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -2,12 +2,11 @@
 #include "limits.h"
 
 /**
- * is_bst_helper - Checks if a binary tree is a valid binary search tree.
- * @tree: A pointer to the root node of the tree to check.
- * @lo: The value of the smallest node visited thus far.
- * @hi: The value of the largest node visited this far.
- *
- * Return: If the tree is a valid BST, 1, otherwise, 0.
+ * is_bst_helper - helper to check check if binary tree is a binary search tree
+ * @tree: root of tree
+ * @lo: lowest value so far
+ * @hi: highest value so far
+ * Return: if the tree is a valid BST 1, else 0
  */
 int is_bst_helper(const binary_tree_t *tree, int lo, int hi)
 {
@@ -22,10 +21,9 @@ int is_bst_helper(const binary_tree_t *tree, int lo, int hi)
 }
 
 /**
- * binary_tree_is_bst - Checks if a binary tree is a valid binary search tree.
- * @tree: A pointer to the root node of the tree to check.
- *
- * Return: 1 if tree is a valid BST, and 0 otherwise
+ * binary_tree_is_bst - check if a binary tree is a valid binary search tree
+ * @tree:  root node of tree to check
+ * Return: 1 if tree is a valid BST, else 0
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
