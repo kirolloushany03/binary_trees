289982b01d852d7903b9c65c08f2fa025b19185c
ali-hy <ali.haitham.ymy@gmail.com>
2024-03-29 01:09:01 +0200

    refactor 103

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,34 +1,33 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_rotate_left - Left-rotates a binary tree.
- * @tree: A pointer to the root node of the tree to rotate.
- *
- * Return: A pointer to the new root node after rotation.
+ * binary_tree_rotate_left - left-rotates a binary tree
+ * @tree: node at root of tree to be rotated
+ * Return: pointer to new root after rotation
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-	binary_tree_t *pivot, *tmp;
+	binary_tree_t *rot_piv, *node_temp;
 
 	if (tree == NULL || tree->right == NULL)
 		return (NULL);
 
-	pivot = tree->right;
-	tmp = pivot->left;
-	pivot->left = tree;
-	tree->right = tmp;
-	if (tmp != NULL)
-		tmp->parent = tree;
-	tmp = tree->parent;
-	tree->parent = pivot;
-	pivot->parent = tmp;
-	if (tmp != NULL)
+	rot_piv = tree->right;
+	node_temp = rot_piv->left;
+	rot_piv->left = tree;
+	tree->right = node_temp;
+	if (node_temp != NULL)
+		node_temp->parent = tree;
+	node_temp = tree->parent;
+	tree->parent = rot_piv;
+	rot_piv->parent = node_temp;
+	if (node_temp != NULL)
 	{
-		if (tmp->left == tree)
-			tmp->left = pivot;
+		if (node_temp->left == tree)
+			node_temp->left = rot_piv;
 		else
-			tmp->right = pivot;
+			node_temp->right = rot_piv;
 	}
 
-	return (pivot);
+	return (rot_piv);
 }
